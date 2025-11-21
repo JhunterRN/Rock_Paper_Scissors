41593a6adefa390bfc2ffbf5783c5e1302acb2f9
Josh Hunter <Jhunter.RN@outlook.com>
2025-11-21 12:53:35 -0700

    style: add simple html/css styling for game

--- rps.js
+++ rps.js
@@ -4,3 +4,4 @@
 //     >Create function that accepts said input string including only "rock", "Paper", or "scissors" are case insensitive as values.
 //     >Assign resulted value to a variable (Human Choice)
 
+let getHumanChoice = document.getElementsByClassName("")
