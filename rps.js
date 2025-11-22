b3535a45bd976ced0945351a57a9b601b86f5109
Josh Hunter <Jhunter.RN@outlook.com>
2025-11-21 19:58:26 -0700

    feat:add human choice promt and variable
    Previous commit cleaned up pseudocode and
    changed order of layout. Removed complexity
    until understanding obtained.

--- rps.js
+++ rps.js
@@ -5,9 +5,11 @@
 //     >If the value is three choose 'scissors'
 // Assign returned value to a variable (Computer Choice)
 
-let randomNumber = Math.floor(Math.random() * 3) +1;
+let randomNumber = Math.floor(Math.random() * 3) +1; // Creates a random intiger from 1-3.
 
-console.log(randomNumber)
+console.log(randomNumber)  //Log here to check functionality of randomNumber. 
+
+// Now a function that utilizes the randomNumber variable to choose rock, paper, or scissors. 
 
 function getComputerChoice() {
     const choice = randomNumber;
@@ -20,6 +22,27 @@ function getComputerChoice() {
     }
 }
 
-let computerChoice = getComputerChoice();
+let computerChoice = getComputerChoice(); // Assigning the previous function to a variable.
 
 console.log(computerChoice);
+
+// When the user inputs "rock", "paper", or "scissors": (Human player's Choice)
+//     >Create function that accepts said input string including only "rock", "Paper", or "scissors" are case insensitive as values. 
+//     >Assign resulted value to a variable (Human Choice)
+
+function getHumanChoice() {
+    let decision = prompt("Rock, Paper, or Scissors?");
+    if (decision === "Scissors") {
+        return "scissors";
+    } else if 
+        (decision === "rock") {
+        return "rock";
+        } else {
+        return "paper";
+    }
+       
+}
+
+let humanChoice = getHumanChoice(); // Assigning the previous function to a variable. 
+
+console.log(humanChoice);
